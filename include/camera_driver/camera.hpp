d0d75fdcdc4817801af28d41bfcd56ac3b219a23
Aaron Yoon <awesomeyooner@gmail.com>
2026-06-20 23:09:36 +0000

    Started adding methods to hpp

--- include/camera_driver/camera.hpp
+++ include/camera_driver/camera.hpp
@@ -18,6 +18,44 @@ class Camera
 
     public:
 
+        /**
+         * @brief Create a new VideoCapture wrapper with the given camera index.
+         * The name will automatically be set to `camera_<index>`
+         * 
+         * You can find the index of your camera by using 
+         * 
+         * `$ ls -l /dev/v4l/by-id/`
+         * 
+         * Example:
+         * 
+         * `Camera(0) -> m_name = "camera_0"`
+         * 
+         * @param index `int` The index of the camera
+         * @param api_type `cv::VideoCaptureAPIs` The API Type to use, defaults to `cv::CAP_ANY`
+         */
+        Camera(int index, cv::VideoCaptureAPIs api_type = cv::CAP_ANY);
+
+        /**
+         * @brief Create a new VideoCapture wrapper at the given path.
+         * The name will automatically be set to `camera_<path>`
+         * 
+         *  * You can find the path of your camera by using 
+         * 
+         * `$ ls -l /dev/v4l/by-path/`
+         * 
+         * Example:
+         * 
+         * `Camera("/dev/v4l/by-path/pci-0000:05:00.3-usb-0:4:1.0-video-index0") -> m_path = "camera_/dev/v4l/by-path/pci-0000:05:00.3-usb-0:4:1.0-video-index0`
+         * 
+         * @param path `std::string` The path of the device, usually in `/dev/...`
+         * @param api_type `cv::VideoCaptureAPIs` The API Type to use, defaults to `cv::CAP_ANY`
+         */
+        Camera(std::string path, cv::VideoCaptureAPIs api_type = cv::CAP_ANY);
+
+        
+
+
+
     private:
 
         // The OpenCV backend device
